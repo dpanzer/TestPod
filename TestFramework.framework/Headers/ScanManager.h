b1ee2b8af1116b3e285630d8cf6e6815459d3a83
Danny Panzer <danny.panzer@gmail.com>
2016-07-25 14:20:28 -0400

    first version with ocr

---
+++ TestFramework.framework/Headers/ScanManager.h
@@ -0,0 +1,17 @@
+//
+//  ScanManager.h
+//  TestFramework
+//
+//  Created by Danny Panzer on 7/18/16.
+//  Copyright © 2016 DFP Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ScanManager : NSObject
+
++(instancetype) sharedManager;
+
+
+@end
